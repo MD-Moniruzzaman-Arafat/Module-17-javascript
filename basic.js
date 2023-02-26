28a1e8a856380eff9ad0c007cfaa372ec667b57d
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2023-02-26 23:53:35 +0600

    basic js

---
+++ basic.js
@@ -0,0 +1,23 @@
+// console.log(7);
+var number = 10.00;
+// var name = "hello world";
+// console.log(name);
+// console.log(number);
+// console.log(10 + 10);
+
+var name1 = "monir";
+var name2 = "arafat";
+console.log(name1 + " " + name2);
+var numString = "3.14";
+var num = parseFloat(numString);
+console.log(num); // Output: 3.14
+
+var numString1 = "123";
+var num1 = parseInt(numString1);
+console.log(num1); // Output: 123
+
+var numbers = 10.65987659;
+console.log(numbers.toFixed(2));
+
+
+
